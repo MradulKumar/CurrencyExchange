0d35a129e038bc2d62b4550e4fd79a9d5801a108
MradulKumar <mradul.iimt@gmail.com>
2024-09-16 13:45:59 +0530

    Initial Commit

---
+++ CurrencyExchange/CurrencyExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangeApp.swift
+//  CurrencyExchange
+//
+//  Created by Mradul Kumar on 16/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
